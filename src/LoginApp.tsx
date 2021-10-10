a51a6101309cbb29c25b3ac9ff9db0820ee2b335
? <54632693+arch-m@users.noreply.github.com>
2021-10-09 21:08:46 -0500

    First commit

---
+++ src/LoginApp.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Login } from './pages/login/Login';
+
+export const LoginApp = () => {
+  return (
+    <Login />
+  );
+}
---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { LoginForm } from './components/LoginForm';
+
+export const Login = () => {
+  return (
+    <div>
+      <h1>LoginPage</h1>
+      <LoginForm />
+    </div>
+  );
+}
